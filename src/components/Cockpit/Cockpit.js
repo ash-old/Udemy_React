a972bb57e12e8424dd3ddf07c61d6fda992ebfea
ash-old <ashley.oldham@gmail.com>
2021-01-21 01:13:17 +0000

    issue fixed

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import classes from './Cockpit.css';
+
+const cockpit = (props) => {
+
+    const assignedClasses = [];
+    let btnClass = '';
+    if(props.showPerson) {
+      btnClass= classes.Red;  
+    }
+    
+    if (props.persons.length <= 2) {
+      assignedClasses.push('red');
+    }
+    if(props.persons.length <= 1) {
+        assignedClasses.push('bold');
+    }
+
+    return (
+        <div className={'Cockpit'}>
+           <h1>Hello World!</h1>
+        <p className={assignedClasses.join(' ')}>This is really working!</p>
+        <button 
+            className={btnClass} 
+            onClick={props.toggle} >Toggle Persons</button> 
+        </div>
+        
+    );
+};
+
+export default cockpit;
---
+++ src/containers/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import classes from './App.css';
+import Persons from '../components/Persons/Persons';
+import Cockpit from '../components/Cockpit/Cockpit';
+
+class App extends Component {
+  state = {
+    persons: [
+      {id: 1, name: 'Ash', age: 39},
+      {id: 2, name: 'Moo', age: 5},
+      {id: 3, name: "Sho", age: 34}
+    ],
+    showPersons: false
+  }
+
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons.slice();
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
+  }
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id
+    })
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons})
+  }
+
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
+  render() {
+   
+    let persons = null;
+
+    if(this.state.showPersons) {
+      persons = <Persons 
+            persons={this.state.persons}
+            clicked={this.deletePersonHandler}
+            changed={this.nameChangedHandler}
+          />
+    }
+
+    return (
+      <div className='App'>
+        <Cockpit showPersons={this.state.showPersons}
+                 persons={this.state.persons}
+                 toggle={this.togglePersonHandler}
+        />
+        {persons}
+      </div>
+    );
+  }
+}
+
+export default App;
